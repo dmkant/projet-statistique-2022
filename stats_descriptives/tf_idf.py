c47e3af56554d3ca08c2d621f49d706fd24897c7
atero18 <atero18@gmail.com>
2022-03-09 15:23:16 +0100

    Représentation vectorielle de documents via moyenne des mots

---
+++ stats_descriptives/tf_idf.py
@@ -0,0 +1,75 @@
+from typing import Union
+
+import numpy as np
+
+
+def TF(doc: list[str], vocabulaire: Union[list, set] = None, type: str = 'frequence_brute', **kwargs) -> dict:
+
+    if vocabulaire is not None:
+        vocabulaire = set(vocabulaire).union(set(doc))
+    
+    else:
+        vocabulaire = set(doc)
+
+    frequenceBrute = {v: 0 for v in vocabulaire}
+
+    for mot in doc:
+        frequenceBrute[mot] += 1
+
+    frequenceBrute = {v: frequenceBrute[v] / len(doc) for v in vocabulaire}
+
+    if type == 'frequence_brute':
+        return frequenceBrute
+
+    elif type == 'binaire':
+        return {v: frequenceBrute[v] > 0 for v in vocabulaire}
+    
+    elif type == 'normalisation':
+        K: float = kwargs['K'] if 'K' in kwargs else .5
+
+        maximum:float = max(frequenceBrute.values())
+        return {v: K + (1 - K) * frequenceBrute[v] / maximum for v in vocabulaire}
+
+def TF_docs(docs: list[list[str]], vocabulaire: Union[list, set] = None, type: str = 'frequence_brute', **kwargs) -> list[dict]:
+
+    tfDocs = []
+    for doc in docs:
+       tfDocs.append(TF(doc, vocabulaire, type, **kwargs))
+    
+    return tfDocs
+
+def IDF(docs: list[list[str]], vocabulaire: Union[list, set] = None):
+
+    vocabulairePresentDocuments = set()
+    for doc in docs:
+        vocabulairePresentDocuments.update(set(doc))
+
+    if vocabulaire is not None:
+        vocabulaire = set(vocabulaire).union(vocabulairePresentDocuments)
+
+    else:
+        vocabulaire = vocabulairePresentDocuments
+
+    idf = {v: np.inf for v in vocabulaire}
+    frequence = {v: sum(v in doc for doc in docs) for v in vocabulaire}
+    
+    for mot in vocabulaire:
+        if frequence[mot] > 0:
+            idf[mot] = np.log(len(docs) / frequence[mot])
+
+    
+    return idf
+
+def TF_IDF(docs: list[list[str]], vocabulaire: Union[list, set] = None) -> list[dict]:
+
+    idf = IDF(docs, vocabulaire)
+    
+    vocabulaire = set(idf.keys())
+
+    retour = []
+    for doc in docs:
+        tf = TF(doc, vocabulaire)
+        tfidf = {v: tf[v] * idf[v] for v in vocabulaire}
+        retour.append(tfidf)
+
+    return retour
