bdad7eb9621a4f22d377ead314ed1d19aba24492
Hugo Miccinilli <hugo.miccinilli@gmail.com>
2022-03-20 20:22:04 +0100

    add doc_embedding dir

---
+++ doc_embedding/moyenne.py
@@ -0,0 +1,46 @@
+import sys
+
+import numpy as np
+from gensim import models
+from typing import List
+
+
+sys.path.append('..')
+from stats_descriptives import tf_idf as TFIDF
+
+
+def word_emb_vers_doc_emb_moyenne(docs: List[List[str]], modele: models.KeyedVectors, methode: str = 'TF-IDF') -> List[np.ndarray]:
+
+    moyennes = []
+    if methode == 'TF-IDF':
+        ponderations = TFIDF.TF_IDF(docs)
+        # Les pondérations ne valent pas forcément 1 en somme,
+        # on normalise
+        for i in range(len(docs)):
+            somme = sum(ponderations[i].values())
+            if somme != 1:
+                #[print(ponderations[i][v]) for v in ponderations[i]]
+                ponderations[i] = {v: ponderations[i][v] / somme for v in ponderations[i]}
+
+    elif methode == 'TF':
+        # La somme des pondérations vaut 1 par définition
+        ponderations = TFIDF.TF_docs(docs)
+
+    for i in range(len(docs)):
+
+        representation: np.ndarray = None
+
+        for v in ponderations[i]:
+
+            if ponderations[i][v] != 0:
+
+                if representation is None:
+                    representation = ponderations[i][v] * modele[v]
+
+                else:
+                    representation += ponderations[i][v] * modele[v]
+
+        moyennes.append(representation)
+   
+   
+    return moyennes
